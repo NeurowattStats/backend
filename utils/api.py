e698ad0af18dd46933bc39599e8e9655d0d35241
JasonWang913 <jason@neurowatt.ai>
2025-01-02 18:02:43 +0800

    feat: 財報RAG相關的api
    - router多新增一個路徑 `/neurostats/RAG`
    而財報RAG為在此之上的`/financial_report`
    以及 `/financial_report/query`
    
    - /financial_report為初始化query_engine (第一次點擊頁面用)
      `/financial_report/query` 則為發送query並得到LLM回覆

--- utils/api.py
+++ utils/api.py
@@ -1,18 +1,19 @@
 from fastapi import HTTPException
 
+
 def handle_request(
-        ticker, 
-        service_class, 
-        method_name, 
-        include_content=False, 
+        ticker,
+        service_class,
+        method_name,
+        include_content=False,
         content_method_name=None  # 新增參數 content_method_name，默認為 None
-    ):
+):
     """Helper function to handle request and response generation."""
     try:
         responser = service_class(ticker)
         method = getattr(responser, method_name)
         data = method()
-        
+
         # 如果 include_content 為 True 且 content_method_name 被提供
         if include_content:
             # 如果沒有指定 content_method_name，則根據 method_name 默認推導
@@ -24,8 +25,48 @@ def handle_request(
                 content = content_method()
                 return {'content': content, 'array': data}
             else:
-                raise AttributeError(f"No content method found for {content_method_name}")
-        
+                raise AttributeError(
+                    f"No content method found for {content_method_name}")
+
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+def handle_RAG_query(
+        ticker,
+        year,
+        season,
+        top_k,
+        question,
+        service_class,
+        method_name,
+        include_content=False,
+        content_method_name=None  # 新增參數 content_method_name，默認為 None
+):
+    """
+    RAG頁面相關的request,
+    輸入需要ticker, year 和 season指定某一份財報
+    輸入top_k 決定參考前幾名的文件
+    """
+    try:
+        responser = service_class(ticker, year, season, top_k, question)
+        method = getattr(responser, method_name)
+        data = method()
+        # 如果 include_content 為 True 且 content_method_name 被提供
+        if include_content:
+            # 如果沒有指定 content_method_name，則根據 method_name 默認推導
+            if content_method_name is None:
+                content_method_name = f"{method_name}_text"
+
+            if hasattr(responser, content_method_name):
+                content_method = getattr(responser, content_method_name)
+                content = content_method()
+                return {'content': content, 'array': data}
+            else:
+                raise AttributeError(
+                    f"No content method found for {content_method_name}")
+
         return data
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
