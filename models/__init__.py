840a06e81fda308dba0038031fd6011bc088d03b
JasonWang913 <jason@neurowatt.ai>
2025-01-20 12:23:13 +0800

    feat: 新增TEJ 相關資料功能
    - 取得TEJ 會計師財務資料的數值資料

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 from .request import TickerRequest
-from .fundamental import (OverviewModel, PerShareModel, ProfitabilityModel,
-                          GrowthMomentumModel, FinancialResilienceModel,
-                          BalanceSheetModel, OperatingIndicatorsModel)
+from .fundamental import (
+    OverviewModel, PerShareModel, ProfitabilityModel, GrowthMomentumModel, FinancialResilienceModel, BalanceSheetModel,
+    OperatingIndicatorsModel)
 from .valuation import ValuationOverview
 from .tech import TechBasicIndexes, TechDailyIndexes, TechIndexes, TechIndexesList
 from .chip import ChipOverall
 from .common import TitleArray
 from .financial_report import QueryInput, QueryResponse
 from .earnings_call import EarningsCallRequest
+from .tej import TEJCompanySelfSettlementResponse, TEJFinancetStatementResponse
